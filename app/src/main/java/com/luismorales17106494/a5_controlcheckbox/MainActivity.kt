39ae045a9246ce99d5ff483681a0912b5a624b83
Edu-Lu <moralesluis17106494@gmail.com>
2022-06-20 15:06:36 -0500

    Ejercicio CheckBox sin resolver

---
+++ app/src/main/java/com/luismorales17106494/a5_controlcheckbox/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.luismorales17106494.a5_controlcheckbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+        5 - Control CheckBox
+
+     El objetivo de este concepto es seguir practicando lo visto hasta
+     ahora para la creación de un proyecto con Android Studio e incorporar
+     el control visual CheckBox
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
